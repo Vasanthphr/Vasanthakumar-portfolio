90638ae2e9e0c4db18b73dbe6c73c84027d6df51
vasanthphr <vagsanth@gmail.com>
2022-11-01 11:49:01 +0530

    2nd commit

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,5 +1,17 @@
 function Skills() {
-return<div>Skills</div>    
+return <div className="container">
+    <div className="row">
+        <div className="col-lg-12">
+        <div class="card" style={{width:"15rem"}}>
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        </div>
+    </div>
+</div>
 }
 
 export default Skills
